1c6139d789c158f14a4e0d8db1a4c13a6be2884c
Darsh Mangukiya <103213378+Darsh76@users.noreply.github.com>
2025-04-22 18:30:59 +0530

    Update vite.config.js

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -6,8 +6,8 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,8 +15,8 @@ export default defineConfig({
   server: {
     host: true, // or '0.0.0.0'
     port: 5173, // or your Vite port
-    allowedHosts: ['tuvocdarsh.developmentlabs.co'],
+    allowedHosts: ['tuvocdarsh.developmentlabs.co'], // Add your domain here
     strictPort: true, // optional but helps with debugging
   },
-  },
-})
+});
+)
