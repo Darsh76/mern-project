8fd0ea8401b7b27e7be2d9a5e4898ccaf86d59fd
Darsh Mangukiya <103213378+Darsh76@users.noreply.github.com>
2025-04-22 17:40:16 +0530

    Update vite.config.js

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -13,6 +13,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
-  }
+    allowedHosts: ['tuvocdarsh.developmentlabs.co'],
+  },
 })
